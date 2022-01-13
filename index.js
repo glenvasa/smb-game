0907531529ec8c75842fc9cc6f1e90ff36ea72e8
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-13 11:37:55 -0500

    added window eventlisteners for keydown and keyup for movement upwards and left/right

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y;
 
     if(this.position.y + this.height + this.velocity.y <= canvas.height){
@@ -40,11 +41,60 @@ class Player {
 
 const player = new Player();
 
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+}
+
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   player.update();
   
+  if (keys.right.pressed) {
+      player.velocity.x = 5
+  } else if (keys.left.pressed) {
+      player.velocity.x = -5
+  } else player.velocity.x = 0
+
 }
 
 animate();
+
+addEventListener('keydown', ({keyCode}) => {
+    switch(keyCode){
+        case 65: // 'a' left
+         keys.left.pressed = true
+            break 
+        case 83: // 's' down
+            console.log('down');
+            break 
+        case 68: // 'd' right
+            keys.right.pressed = true
+            break 
+        case 87: // 'w' up
+            player.velocity.y -= 20
+            break 
+    }
+})
+
+addEventListener('keyup', ({keyCode}) => {
+    switch(keyCode){
+        case 65: // 'a' left
+        keys.left.pressed = false
+            break 
+        case 83: // 's' down
+            console.log('down');
+            break 
+        case 68: // 'd' right
+        keys.right.pressed = false
+            break 
+        case 87: // 'w' up
+            // player.velocity.y -= 20
+            break 
+    }
+})
