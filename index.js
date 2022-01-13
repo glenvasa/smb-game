13f7fe9dec4f59130bd641e8e6298a5aaf14d8fb
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-13 10:33:58 -0500

    created Player class

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const canvas = document.querySelector('canvas')
 
 const ctx = canvas.getContext('2d')
-console.log(ctx);
+canvas.width = window.innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    
+}
+
+const player = new Player()
+player.draw()
