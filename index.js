0e7942532b86b0ea5398ec6f22e4ae3d627777ee
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-01-15 11:58:02 -0500

    created Platform class and code in animate() to stop player's y velocity when sitting on top of platform only

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const canvas = document.querySelector("canvas");
 
 const ctx = canvas.getContext("2d");
@@ -39,7 +40,27 @@ class Player {
   }
 }
 
+class Platform {
+  constructor(){
+    this.position = {
+      x: 300,
+      y: 400
+    }
+
+    this.width = 200
+    this.height = 20
+  }
+
+  draw() {
+    ctx.fillStyle = ('blue')
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+
+
 const player = new Player();
+const platform = new Platform()
 
 const keys = {
     right: {
@@ -51,15 +72,25 @@ const keys = {
 }
 
 function animate() {
+ 
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   player.update();
-  
+  platform.draw()
+
   if (keys.right.pressed) {
       player.velocity.x = 5
   } else if (keys.left.pressed) {
       player.velocity.x = -5
   } else player.velocity.x = 0
+
+  // rectangular collision detection (player/platform)
+  if (player.position.y + player.height <= platform.position.y
+    && player.position.y + player.height + player.velocity.y >= platform.position.y 
+    && player.position.x + player.width >= platform.position.x 
+    && player.position.x <= platform.position.x + platform.width){
+    player.velocity.y = 0
+  }
 
 }
 
